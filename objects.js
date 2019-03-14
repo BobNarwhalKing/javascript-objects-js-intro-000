59009a2231496fb9fc3cc0cf5c2c66dc367e8b33
Kurtis Leach <danceflurry4366@gmail.com>
2019-03-14 17:56:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object({Darude: "Sandstorm"})
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  return playlist[artisName] = songTitle
+  return playlist[artistName] = songTitle
 }
 function removeFromPlaylist (playlist, artistName) {
   return delete playlist.artistName
